fa78aa23db471b4c93f8723396cc448c835d9ffc
Jorge Cortes <77340382+chochelius@users.noreply.github.com>
2023-11-27 00:32:49 -0300

    ultimo commit 27-11

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -2,11 +2,20 @@ import './Splash.css';
 
 const Splash = () => {
 
+const testSplash = () => {
+    console.log("test-splash");
+}
+
+testSplash();
+
+
+
     return (
         <div className='splash'>
             <div className='p-5'>
                 <h1 className='display-3 mx-auto'>ClimateCoachGPT</h1>
                 <p className='lead'>Climate Coach GPT is a tool designed to help you improve your climate awareness.</p>
+                <img src="img1.png" alt="" className='spinner-grow splash-img' />
             </div>
         </div>
     );
