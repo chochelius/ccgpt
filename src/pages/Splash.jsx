e423569ce4e77e6d8b6bcf8f26e52a3700bb7e1f
Jorge Cortes <77340382+chochelius@users.noreply.github.com>
2023-11-27 00:07:02 -0300

    created splash

---
+++ src/pages/Splash.jsx
@@ -0,0 +1,15 @@
+import './Splash.css';
+
+const Splash = () => {
+
+    return (
+        <div className='splash'>
+            <div className='p-5'>
+                <h1 className='display-3 mx-auto'>ClimateCoachGPT</h1>
+                <p className='lead'>Climate Coach GPT is a tool designed to help you improve your climate awareness.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Splash;
