02cc98cdb04dc6fef6cc0bd256d42ea34224473e
Jorge Cortes <chochelius@gmail.com>
2023-12-01 14:13:30 -0300

    footer funcional

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,21 +5,21 @@ const Footer = () => {
     <footer className="fixed-bottom">
       <div className="row mt-3 pt-4 pb-2 bg-body-tertiary">
         <div className="col-4 text-center text-success h1">
-          <Link to="/"> {/* BEGIN: Add route to "/" */}
+          <Link to="/" className="text-success"> {/* BEGIN: Add route to "/" */}
             <div className="text-center">
               <i className="bi bi-house fw-bold"></i>
             </div>
           </Link> {/* END: Add route to "/" */}
         </div>
         <div className="col-4 text-center text-success h1">
-          <Link to="/data"> {/* BEGIN: Add route to "/data" */}
+          <Link to="/data" className="text-success"> {/* BEGIN: Add route to "/data" */}
             <div className="text-center">
               <i className="bi bi-file-bar-graph"></i>
             </div>
           </Link> {/* END: Add route to "/data" */}
         </div>
-        <div className="col-4 text-center text-success h1">
-          <Link to="/chat"> {/* BEGIN: Add route to "/chat" */}
+        <div className="col-4 text-center  h1">
+          <Link to="/chat" className="text-success"> {/* BEGIN: Add route to "/chat" */}
             <div className="text-center">
               <i className="bi bi-chat-square-text"></i>
             </div>
